fa13036db005838ad362cf94c36dbc3cb1b1b757
olegsugatov <oleg.sugatov@gmail.com>
2017-05-12 23:41:08 +0300

    scrollmagic debug.addIndicators

--- README.md
+++ README.md
@@ -1,1 +1,13 @@
 # scrollmagic
+http://www.janpaepke.de
+http://scrollmagic.io/
+http://www.transformicons.com/
+# scrollmagic examples
+https://codepen.io/ncerminara/pen/EajxXx
+
+#scrollmagic api
+http://scrollmagic.io/docs/
+
+#scrollmagic plugins
+plugins/debug.addIndicators.js
+http://scrollmagic.io/docs/debug.addIndicators.html
--- src/script.js
+++ src/script.js
@@ -1,10 +1,27 @@
-// init controller 
-var controller = new ScrollMagic.Controller();
+$(document).ready(function(){
+	// init scroll magic
+	var controller = new ScrollMagic.Controller();
+
+	// build a scene
+	var ourScene = new ScrollMagic.Scene({
+		triggerElement: '#project01',
+		duration: 400
+	})
+	.setClassToggle('#project01', 'fade-in')
+	.addIndicators({
+		name: 'fade scene',
+		colorTrigger: 'black',
+		indent: 200,
+		colorStart: '#75C695'
+	})
+	.addTo(controller);
+});
+
  
 // create a scene 
-new ScrollMagic.Scene({
-        duration: 100,	// the scene should last for a scroll distance of 100px 
-        offset: 50		// start this scene after scrolling for 50px 
-    })
-    .setPin("#my-sticky-element") // pins the element for the the scene's duration 
-    .addTo(controller); // assign the scene to the controller 
+// new ScrollMagic.Scene({
+//         duration: 100,	// the scene should last for a scroll distance of 100px 
+//         offset: 50		// start this scene after scrolling for 50px 
+//     })
+//     .setPin("#my-sticky-element") // pins the element for the the scene's duration 
+//     .addTo(controller); // assign the scene to the controller 
